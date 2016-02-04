0052129cc5a3ca67b26a4a1c0e575e99c494da1e
Stevie <steviepinero@gmail.com>
2016-02-04 14:55:18 -0500

    siskel api project

--- siskel.rb
+++ siskel.rb
@@ -1,10 +1,36 @@
 require 'httparty'
 
 class Siskel
-  attr_reader :title
+  attr_accessor :title, :rating, :year, :plot, :options, :tomato
+   def initialize(title, options = {})
+     @options = options
+     url = 'http://www.omdbapi.com/?' + stuff(title, @options)
+     movie = HTTParty.get(url)
+     @tomato = movie['tomatoMeter'].to_i
+     @title = movie['Title'] || 'Movie not found!'
+     @rating = movie['Rated']
+     @year = movie['Year']
+     @plot = movie['Plot']
+   end
 
-  def initialize(title)
-    @movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}")
-    @title = @movie['Title']
+  def stuff(title, stuff)
+    parameters = []
+    parameters.push "t=#{title}"
+    parameters.push 'tomatoes=true'
+    parameters.push "y=#{options[:year]}" if options[:year]
+    parameters.push "plot=#{options[:plot]}" if options[:plot]
+    parameters.join('&')
   end
+
+  def consensus
+    if @tomato.between?(76, 100)
+         'Two Thumbs Up'
+       elsif @tomato.between?(51, 75)
+         'Thumbs Up'
+       elsif @tomato.between?(26, 50)
+         'Thumbs Down'
+       else
+         'Two Thumbs Down'
+       end
+     end
 end
